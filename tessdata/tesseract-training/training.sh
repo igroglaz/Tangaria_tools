094cbb3a470d2d3bca9979b998ea950042638828
Serega <71586060+s88100@users.noreply.github.com>
2020-12-23 00:23:34 +0600

    update tessdata

--- tessdata/tesseract-training/training.sh
+++ tessdata/tesseract-training/training.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # Constants
 FONT_NAME="Tangaria-24X36TG Medium"
+FONT_OUTPUT="tangaria"
 MAX_PAGES=2000
-NUM_ITERATIONS=8000
+NUM_ITERATIONS=10000
 
+TESSTRAIN=1
+RM_OUTPUT=1
+LSTMEVAL=1
+
+if [ $TESSTRAIN = 1 ]; then
 # Remove the previosly generated training data
 rm -rf train/*
 
@@ -19,30 +25,36 @@ tesstrain.sh \
 --save_box_tiff \
 --maxpages $MAX_PAGES \
 --output_dir train
+fi
 
 # Extract the best trainned model
 combine_tessdata -e ./tesseract/tessdata/eng.traineddata eng.lstm
 
-# lstmeval - Evaluation program for LSTM-based networks
-#lstmeval --model output/tangaria_checkpoint \
-#--traineddata tesseract/tessdata/eng.traineddata \
-#--eval_listfile train/eng.training_files.txt
-
 # Fine tune the model
+if [ $RM_OUTPUT = 1 ]; then
 rm -rf output/*
+fi
 OMP_THREAD_LIMIT=8 lstmtraining \
 --continue_from eng.lstm \
---model_output output/tangaria \
+--model_output output/"$FONT_OUTPUT" \
 --traineddata tesseract/tessdata/eng.traineddata \
 --train_listfile ./train/eng.training_files.txt \
 --max_iterations $NUM_ITERATIONS
 
+if [ $LSTMEVAL = 1 ]; then
+# lstmeval - Evaluation program for LSTM-based networks
+lstmeval --model output/"$FONT_OUTPUT"_checkpoint \
+--traineddata tesseract/tessdata/eng.traineddata \
+--eval_listfile train/eng.training_files.txt
+fi
+
 # Combine the checkpoints and create the final model
-lstmtraining --stop_training --continue_from output/tangaria_checkpoint --traineddata ./tesseract/tessdata/eng.traineddata \
---model_output output/tangaria.traineddata
+lstmtraining --stop_training --continue_from output/"$FONT_OUTPUT"_checkpoint \
+--traineddata ./tesseract/tessdata/eng.traineddata \
+--model_output output/"$FONT_OUTPUT".traineddata
 
 echo "mv traineddata..."
-mv -f ./output/tangaria.traineddata ./tangaria_eng.traineddata || {
+mv -f ./output/"$FONT_OUTPUT".traineddata ./"$FONT_OUTPUT"_eng.traineddata || {
     echo "error: not found traineddata..."
     exit 1
 }
